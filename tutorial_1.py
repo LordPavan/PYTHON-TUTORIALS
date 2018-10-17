db2e9eab7d26f93ee24deec5a69981823f27d714
LordPavan <dragon.pavankumar@gmail.com>
2018-10-17 23:38:10 +0530

    tutorial_1.py
    This tutorial teaches you about how to print anything in Python

---
+++ tutorial_1.py
@@ -0,0 +1,6 @@
+#the 'print' statement helps you to print any statement on the output screen
+#first u need to type the keyword 'print' , then put a pair of parantheses, put either single or double quotes and within those 
+#quotes u can type your sentence
+
+print('Hello World!') #using single quotes
+print("Hello World!") #using double quotes
